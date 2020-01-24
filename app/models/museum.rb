806c46cd42ea85383842967c43565d2abc7322b0
munroe1786 <era3munroe@gmail.com>
2020-01-24 10:42:00 -0600

    Add validations to Museum and Review models

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,4 +1,5 @@
 class Museum < ApplicationRecord
+    validates :name, :location, presence: true
     belongs_to :user
     has_many :reviews
     has_many :users, through: :reviews
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+    validates :date_visited, :content, presence: true
+    validates :rating, inclusion: { in: 1..10, message: "Rating must be between 1 and 10" }
     belongs_to :user
     belongs_to :museum
 end
