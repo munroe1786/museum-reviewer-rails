139181c054663fc6b5ed35d818e2ee3b535a62d3
munroe1786 <era3munroe@gmail.com>
2020-01-23 21:29:29 -0600

    Add date_visited, content, rating and foreign_key columns to Review table. Add associations to Review model

--- db/migrate/20200124032301_create_reviews.rb
+++ db/migrate/20200124032301_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.string :date_visited
+      t.text :content
+      t.integer :rating
+      t.integer :user_id, foreign_key: true
+      t.integer :museum_id, foreign_key: true
       t.timestamps
     end
   end
