32dac6033dd1824c1a4a5d9a2bbcde0cd8a45b12
munroe1786 <era3munroe@gmail.com>
2020-01-24 10:46:41 -0600

    Add before_action to Museum controller as well as index, show, new and create actions

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,2 +1,24 @@
 class MuseumsController < ApplicationController
+    before_action :set_museum, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @museums = Museum.all
+    end
+
+    def show
+    end
+
+    def new
+        @museum = Museum.new
+    end
+
+    def create
+        @museum = Museum.new(museum_params)
+        if @museum.save
+            flash[:success] = "Museum sucessfully created"
+            redirect_to museums_path
+        else
+            render :new
+        end
+    end
 end
