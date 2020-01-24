ef315f3f110bbd8b731fdd5bfaaf667766e889ce
munroe1786 <era3munroe@gmail.com>
2020-01-23 21:15:34 -0600

    Add name, location and user_id to Museum table and add associations to Museum model

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,2 +1,5 @@
 class Museum < ApplicationRecord
+    belongs_to :user
+    has_many :reviews
+    has_many :users, through: :reviews
 end
--- db/migrate/20200124030641_create_museums.rb
+++ db/migrate/20200124030641_create_museums.rb
@@ -1,7 +1,9 @@
 class CreateMuseums < ActiveRecord::Migration[6.0]
   def change
     create_table :museums do |t|
-
+      t.string :name
+      t.string :location
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
