cfaf288395941f19fd6bc08e32a84c96f9e1c492
munroe1786 <era3munroe@gmail.com>
2020-01-24 14:14:14 -0600

    Add index, show, new and create routes to Review controller as well as before_action method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+    before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @reviews = Review.all 
+    end
+
+    def show
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            flash[:success] = "Review successfully created"
+            redirect_to reviews_path
+        else
+            render :new
+        end
+    end
 end
