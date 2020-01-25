68b61b876793d9213c6d8d7d9196a227813bd121
munroe1786 <era3munroe@gmail.com>
2020-01-25 16:16:40 -0600

    add devise db table and model / add reviews table / add devise route in config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_030641) do
+ActiveRecord::Schema.define(version: 2020_01_25_220519) do
 
   create_table "museums", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,28 @@ ActiveRecord::Schema.define(version: 2020_01_24_030641) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "date_visited"
+    t.text "content"
+    t.integer "rating"
+    t.integer "user_id"
+    t.integer "museum_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
